f38f69f514a49efa074f0a57427982d6eb0f45df
SIG1R <grsebas04@gmail.com>
2023-12-31 23:00:01 -0500

    upload changes

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,13 +3,14 @@
 
 # Set monitor
 xrandr --output HDMI-1 --mode 1440x900 --left-of eDP-1
+#xrandr --output HDMI-1 --mode 1920x1080 --left-of eDP-1
 
 
 # Active compositor
 picom &
 
 # Set wallpaper
-feh --bg-fill --randomize ~/Pictures/Wallpapers/*
+feh --bg-fill ~/Pictures/Wallpapers/semi\ circle.jpg
 
 # Active notify center
 dunst &
